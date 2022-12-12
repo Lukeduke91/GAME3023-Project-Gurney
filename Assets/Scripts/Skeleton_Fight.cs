ef788e1224490c1e4c106c8e245d7d8c4243c4e3
Lukeduke91 <gurneylj@gmail.com>
2022-12-12 16:06:21 -0500

    Added enemies, the combat screens and a main menu

---
+++ Assets/Scripts/Skeleton_Fight.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Skeleton_Fight : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.name == "Player")
+        {
+            SceneManager.LoadScene("Battle_Skeleton");
+        }
+    }
+}
